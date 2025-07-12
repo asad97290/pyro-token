a08f873822d2b214a4663bb962820b51996c23ed
Asadullah khan <asadmuhammad427@gmail.com>
2025-07-12 16:52:26 +0500

    using 0.8.19 because of push0

--- src/Pyro.sol
+++ src/Pyro.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity 0.8.19; // because fo push0
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
